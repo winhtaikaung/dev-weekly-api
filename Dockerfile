0a85ce564bbad40ab060016f6f07dbb6b18f3ac1
winhtaikaung <winhtaikaung28@hotmail.com>
2018-04-21 11:25:24 +0800

    docker DB host env added

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,7 @@ COPY requirements.txt /usr/src/app/
 RUN pip install -r requirements.txt
 
 COPY . /usr/src/app
+ENV DB_HOST=192.168.0.110
 COPY run.sh /usr/src/app/
 EXPOSE 5000
 
--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
-docker build ./ -t dev_weekly_api
-docker stop dev_weekly_api || true && docker rm dev_weekly_api||true & docker run -p 80:5000  --name dev_weekly_api dev_weekly_api
+docker build --network="host" ./ -t dev_weekly_api
+
+docker stop dev_weekly_api || true && docker rm dev_weekly_api||true & docker run -p 80:5000 --name dev_weekly_api dev_weekly_api
